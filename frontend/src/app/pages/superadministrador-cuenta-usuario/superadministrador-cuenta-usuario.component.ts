ef1ac7ce311b9e2335c5b7377995d13a039bce36
RaulVillanueva <A16001061@alumnos.uady.mx>
2023-04-04 00:05:49 -0600

    Front-7 Se creo Cuenta Usuario

---
+++ frontend/src/app/pages/superadministrador-cuenta-usuario/superadministrador-cuenta-usuario.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-superadministrador-cuenta-usuario',
+  templateUrl: './superadministrador-cuenta-usuario.component.html',
+  styleUrls: ['./superadministrador-cuenta-usuario.component.css']
+})
+export class SuperadministradorCuentaUsuarioComponent {
+
+}
