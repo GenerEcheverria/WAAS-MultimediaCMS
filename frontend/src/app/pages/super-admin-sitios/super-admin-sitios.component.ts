959721ef402d226ebd5c457f5778b78a7dd84a32
Julián Pérez <japk2001@gmail.com>
2023-04-07 15:18:47 -0600

    Front-6 Tabla completa
    Se añadio el uso de datatables

--- frontend/src/app/pages/super-admin-sitios/super-admin-sitios.component.ts
+++ frontend/src/app/pages/super-admin-sitios/super-admin-sitios.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
+import 'datatables.net';
+import 'datatables.net-bs5';
 
 @Component({
   selector: 'app-super-admin-sitios',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./super-admin-sitios.component.css']
 })
 export class SuperAdminSitiosComponent {
-
+  ngAfterViewInit() {
+    $(document).ready(function() {
+      $('#example').DataTable({
+        
+        "language": {
+          "search": "",
+          "searchPlaceholder": "Buscar",
+      },
+          "dom": '<"d-flex justify-content-end"f>t<"d-flex justify-content-between"ipl>',
+          
+          "pagingType": "simple_numbers",
+      });
+  });
+}
 }
