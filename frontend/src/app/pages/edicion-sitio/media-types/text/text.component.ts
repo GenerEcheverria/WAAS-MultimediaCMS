3b5cf1bf4338104465af5fc949fc2a2bd1b5623c
Gener Echeverría <56045434+GenerEcheverria@users.noreply.github.com>
2023-04-08 19:50:30 -0600

    Front-10 Añadir visibilidad texttype

--- frontend/src/app/pages/edicion-sitio/media-types/text/text.component.ts
+++ frontend/src/app/pages/edicion-sitio/media-types/text/text.component.ts
@@ -1,10 +1,13 @@
 // Dentro de text.component.ts
 import { Component,} from '@angular/core';
+import { ControlContainer, FormGroupDirective } from '@angular/forms';
 
 @Component({
   selector: 'app-text',
   templateUrl: './text.component.html',
-  styleUrls: ['./text.component.css']
+  styleUrls: ['./text.component.css'],
+  viewProviders: [{ provide: ControlContainer, useExisting: FormGroupDirective }]
+
 })
 export class TextComponent {
 }
