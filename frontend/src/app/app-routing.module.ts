1b3ad64193bcce570b031063c3f30c1c4843d633
Gener Echeverría <56045434+GenerEcheverria@users.noreply.github.com>
2023-04-14 16:28:27 -0600

    update

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { TimelineComponent } from './pages/crear-sitio/media-types/timeline/time
 import { FormsModule } from '@angular/forms';
 import { SitiosComponent } from './pages/sitios/sitios.component';
 import { CrearSitioComponent } from './pages/crear-sitio/crear-sitio.component';
-import { LienzoPaginaComponent } from './pages/crear-sitio/lienzo-pagina/lienzo-pagina.component';
 import { SuperAdminSitiosComponent } from "./pages/super-admin-sitios/super-admin-sitios.component";
 import { MiCuentaComponent } from "./pages/mi-cuenta/mi-cuenta.component";
 import { SuperadministradorCuentaUsuarioComponent } from './pages/superadministrador-cuenta-usuario/superadministrador-cuenta-usuario.component';
@@ -21,7 +20,6 @@ const routes: Routes = [
     children: [
       { path: 'sitios', component:  SitiosComponent},
       { path: 'crear', component: CrearSitioComponent},
-      { path: 'lienzo', component: LienzoPaginaComponent},
       { path: 'sasitios', component: SuperAdminSitiosComponent},
       { path: 'mi-cuenta', component: MiCuentaComponent},
       { path: 'sausuarios', component:  SuperadministradorCuentaUsuarioComponent},
