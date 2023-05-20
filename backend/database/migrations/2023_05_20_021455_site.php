d7636ad563d7ebba793d3999403938429355f2f5
Gener Echeverría <56045434+GenerEcheverria@users.noreply.github.com>
2023-05-19 20:22:59 -0600

    API-1 site schema created

---
+++ backend/database/migrations/2023_05_20_021455_site.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('sites', function (Blueprint $table) {
+            $table->increments('id')->unique();
+            $table->string('name');
+            $table->integer('views');
+            $table->unsignedBigInteger('idUser'); 
+            $table->string('url');
+            
+            $table->foreign('idUser')->references('id')->on('users'); 
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('sites');
+    }
+};
+
