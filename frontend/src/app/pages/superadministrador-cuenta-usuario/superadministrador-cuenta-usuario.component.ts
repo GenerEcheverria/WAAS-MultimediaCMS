f3714f0db9d1e6b3bbf55d2d82641be659ea470c
Julián Pérez <japk2001@gmail.com>
2023-05-20 20:28:35 -0600

    SuperadministradorCuentaUsuario documentado

--- frontend/src/app/pages/superadministrador-cuenta-usuario/superadministrador-cuenta-usuario.component.ts
+++ frontend/src/app/pages/superadministrador-cuenta-usuario/superadministrador-cuenta-usuario.component.ts
@@ -3,12 +3,20 @@ import * as $ from 'jquery';
 import 'datatables.net';
 import 'datatables.net-bs5';
 
+/**
+ * Componente para la administración de cuentas de usuario por parte de un superadministrador.
+ */
 @Component({
   selector: 'app-superadministrador-cuenta-usuario',
   templateUrl: './superadministrador-cuenta-usuario.component.html',
   styleUrls: ['./superadministrador-cuenta-usuario.component.css']
 })
 export class SuperadministradorCuentaUsuarioComponent {
+
+  /**
+   * Método que se ejecuta después de que la vista ha sido inicializada.
+   * Inicializa el plugin DataTables para la tabla de cuentas de usuario.
+   */
     ngAfterViewInit() {
       $(document).ready(function() {
         $('#example').DataTable({
