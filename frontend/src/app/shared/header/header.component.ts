fb8e5007d8fd8cfe5faef31b1092716b8d157ad0
Julián Pérez <japk2001@gmail.com>
2023-05-20 20:33:32 -0600

    carpeta shared documentada

--- frontend/src/app/shared/header/header.component.ts
+++ frontend/src/app/shared/header/header.component.ts
@@ -1,10 +1,16 @@
 import { Component, Input } from '@angular/core';
 
+/**
+ * Componente del encabezado de la aplicación.
+ */
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+   /**
+   * Nombre para mostrar en el encabezado.
+   */
   @Input() name: string ="";
 }
--- frontend/src/app/shared/sidebar/sidebar.component.ts
+++ frontend/src/app/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+/**
+ * Componente de la barra lateral de la aplicación.
+ */
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
