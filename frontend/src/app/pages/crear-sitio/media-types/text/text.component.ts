a39ec4eb2bb5889c360053f9cfb628a8b9055d45
Julián Pérez <japk2001@gmail.com>
2023-05-20 19:10:51 -0600

    media-types documentado

--- frontend/src/app/pages/crear-sitio/media-types/text/text.component.ts
+++ frontend/src/app/pages/crear-sitio/media-types/text/text.component.ts
@@ -1,6 +1,14 @@
 import { Component, Input,} from '@angular/core';
 import { ControlContainer, FormGroupDirective } from '@angular/forms';
 
+/**
+ * Componente para mostrar un elemento de texto.
+ * 
+ * @component
+ * @selector app-text
+ * @templateUrl ./text.component.html
+ * @styleUrls ['./text.component.css']
+ */
 @Component({
   selector: 'app-text',
   templateUrl: './text.component.html',
@@ -8,6 +16,12 @@ import { ControlContainer, FormGroupDirective } from '@angular/forms';
   viewProviders: [{ provide: ControlContainer, useExisting: FormGroupDirective }]
 })
 export class TextComponent {
+
+  /**
+   * Columna asociada al elemento de texto.
+   * 
+   * @property {string} column
+   */
   @Input() column!: string;
 
   
