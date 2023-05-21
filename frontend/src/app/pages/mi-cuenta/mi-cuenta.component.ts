deb11553ef46e1e60742ce98f91346941ef1a31c
Julián Pérez <japk2001@gmail.com>
2023-05-20 20:04:44 -0600

    mi-cuenta documentado

--- frontend/src/app/pages/mi-cuenta/mi-cuenta.component.ts
+++ frontend/src/app/pages/mi-cuenta/mi-cuenta.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { passwordValidator } from './password.validator';
 
+/**
+ * Componente encargado de administrar la cuenta del usuario.
+ */
 @Component({
   selector: 'app-mi-cuenta',
   templateUrl: './mi-cuenta.component.html',
@@ -9,13 +12,28 @@ import { passwordValidator } from './password.validator';
 })
 export class MiCuentaComponent implements OnInit {
 
+   /**
+   * Formulario para los datos personales del usuario.
+   */
   public form1!: FormGroup;
+
+  /**
+   * Formulario para el cambio de contraseña del usuario.
+   */
   public form2!: FormGroup;
 
+  /**
+   * Constructor del componente MiCuentaComponent.
+   * @param formBuilder Instancia del FormBuilder utilizado para crear los formularios.
+   */
   constructor(private formBuilder: FormBuilder){
 
   }
 
+  /**
+   * Método de ciclo de vida de Angular que se ejecuta al iniciar el componente.
+   * Se encarga de inicializar los formularios y definir las validaciones.
+   */
   ngOnInit(): void {
     this.form1 = this.formBuilder.group({
       nombre: ['', [Validators.required, Validators.maxLength(32), Validators.minLength(10)]],
@@ -30,6 +48,10 @@ export class MiCuentaComponent implements OnInit {
     }, { validator: passwordValidator })
   }
 
+  /**
+   * Método invocado al enviar el formulario.
+   * Imprime en la consola los valores del formulario de datos personales.
+   */
   send(): any {
     console.log(this.form1.value);
   }
