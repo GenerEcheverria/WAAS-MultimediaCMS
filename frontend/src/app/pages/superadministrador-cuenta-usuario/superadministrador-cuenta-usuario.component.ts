e2d37e3f8b8dc221d94a67b1e069d5c3aee39f5c
Gener Echeverría <56045434+GenerEcheverria@users.noreply.github.com>
2023-05-21 20:17:34 -0600

    Front-32 Cargar sitios de un usuario en la tabla

--- frontend/src/app/pages/superadministrador-cuenta-usuario/superadministrador-cuenta-usuario.component.ts
+++ frontend/src/app/pages/superadministrador-cuenta-usuario/superadministrador-cuenta-usuario.component.ts
@@ -5,6 +5,8 @@ import 'datatables.net-bs5';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SiteService } from 'src/app/services/site.service';
 import { UserService } from 'src/app/services/user.service';
+import { DatePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-superadministrador-cuenta-usuario',
@@ -12,27 +14,34 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./superadministrador-cuenta-usuario.component.css']
 })
 export class SuperadministradorCuentaUsuarioComponent implements OnInit {
-  id!:string | null;
-  protected name!:string;
-  protected email!:string;
-  protected phone!:string;
+  id!: string | null;
+  protected name!: string;
+  protected email!: string;
+  protected phone!: string;
+  protected sites: any;
 
-    ngAfterViewInit() {
-      $(document).ready(function() {
-        $('#example').DataTable({
-          
-          "language": {
-            "search": "",
-            "searchPlaceholder": "Buscar",
+  ngAfterViewInit() {
+    $(document).ready(function () {
+      $('#example').DataTable({
+
+        "language": {
+          "search": "",
+          "searchPlaceholder": "Buscar",
         },
-            "dom": '<"d-flex justify-content-end"f>t<"d-flex justify-content-between"ipl>',
-            
-            "pagingType": "simple_numbers",
-        });
+        "dom": '<"d-flex justify-content-end"f>t<"d-flex justify-content-between"ipl>',
+
+        "pagingType": "simple_numbers",
+      });
     });
   }
 
-  constructor(private route: ActivatedRoute, private userService: UserService,private siteService: SiteService, private router: Router){}
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService, 
+    private siteService: SiteService, 
+    private router: Router, 
+    private datePipe: DatePipe
+  ) { }
 
   ngOnInit(): void {
     this.loadData();
@@ -44,18 +53,25 @@ export class SuperadministradorCuentaUsuarioComponent implements OnInit {
     if (this.id) {
       try {
         const response = await this.userService.getUser(this.id.toString()).toPromise();
-         console.log(response)      
-         this.email = response.email;
-         this.name = response.name;
-         this.phone = response.phone;
+        console.log(response)
+        this.email = response.email;
+        this.name = response.name;
+        this.phone = response.phone;
       } catch (error) {
       }
       try {
         const response = await this.siteService.getSitesForUser(this.id.toString()).toPromise();
-        console.log(response) 
-      } catch (error) {}
+        console.log(response)
+        this.sites = response.sites;
+      } catch (error) { }
     } else {
       this.router.navigate(['/sasitios']);
     }
   }
+
+  formatDate(date: string): string {
+    const formattedDate = this.datePipe.transform(date, 'dd/MM/yyyy HH:mm:ss'); 
+    return formattedDate || '';
+  }
+  
 }
